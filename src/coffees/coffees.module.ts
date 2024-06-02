268254fe2588be441350bc442f049755ff2978b0
Jess Sevilleja <jesss@2damcreative.com>
2024-06-03 00:35:28 +0800

    added coffee entity

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
 @Module({
   controllers: [CoffeesController],
   //exports: []
-  //imports: []
+  imports: [TypeOrmModule.forFeature([Coffee])],
   providers: [CoffeesService],
 })
 export class CoffeesModule {}
