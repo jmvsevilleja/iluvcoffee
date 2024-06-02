35397a2cf6c5a33252d81bde5dc12ab804805326
jmvsevilleja <jesss@2damcreative.com>
2024-06-02 15:55:24 +0800

    added nestjs-typeorm typeorm pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 // import { CoffeesController } from './coffees/coffees.controller';
 // import { CoffeesService } from './coffees/coffees.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule,
+
+  TypeOrmModule.forRootAsync({
+    useFactory: () => ({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'pass123',
+      database: process.env.DB_NAME || 'postgres',
+      autoLoadEntities: true,
+      synchronize: process.env.TYPEORM_SYNC === 'true',
+     })
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
