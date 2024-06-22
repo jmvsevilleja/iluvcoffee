1df3fc960286ed6ddbd3285b7ae0c7d4c435df08
Jess Sevilleja <jesss@2damcreative.com>
2024-06-22 21:38:07 +0800

    added factory providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -11,12 +11,20 @@ class ConfigService {}
 class DevelopmentConfigService {}
 class ProductionConfigService {}
 
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    return ['blend45', 'nescafe'];
+  }
+}
+
 @Module({
   controllers: [CoffeesController],
   exports: [CoffeesService],
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   providers: [
     CoffeesService,
+    CoffeeBrandsFactory,
     {
       provide: ConfigService,
       useClass:
@@ -25,6 +33,12 @@ class ProductionConfigService {}
           : ProductionConfigService,
     }, //  custom providers
     { provide: COFFEE_BRANDS, useValue: ['kopiko', 'nescafe'] }, // non class provider tokens
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
+    }, // factory Providers
   ],
 })
 export class CoffeesModule {}
