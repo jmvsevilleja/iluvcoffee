155e431cafc7ad83aa1df2abd4d8805830d1ea3d
Jess Sevilleja <jesss@2damcreative.com>
2024-06-22 19:44:44 +0800

    added custom providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,12 +6,24 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
+
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
 @Module({
   controllers: [CoffeesController],
   exports: [CoffeesService],
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   providers: [
     CoffeesService,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DevelopmentConfigService
+          : ProductionConfigService,
+    }, //  custom providers
     { provide: COFFEE_BRANDS, useValue: ['kopiko', 'nescafe'] }, // non class provider tokens
   ],
 })
