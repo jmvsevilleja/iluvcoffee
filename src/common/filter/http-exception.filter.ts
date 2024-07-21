4cf2e6687866a7b29a97080c0c929e69c332dbd8
Jess Sevilleja <jesss@2damcreative.com>
2024-07-21 22:49:40 +0800

    added message error

--- src/common/filter/http-exception.filter.ts
+++ src/common/filter/http-exception.filter.ts
@@ -7,12 +7,26 @@ export class HttpExceptionFilter<T extends HttpException> implements ExceptionFi
     const response = ctx.getResponse();
     const request = ctx.getRequest();
     const status = exception.getStatus();
-    const errorResponse = {
+
+    // Get the detailed error response
+    const errorResponse = exception.getResponse();
+
+    // Handle the case where errorResponse might be a string or an object
+    const message = typeof errorResponse === 'string'
+    ? errorResponse
+    : (errorResponse as any).message || exception.message;
+
+    const error = typeof errorResponse === 'string'
+    ? errorResponse
+    : (errorResponse as any).error || 'Internal Server Error';
+
+    const customErrorResponse = {
       statusCode: status,
       timestamp: new Date().toISOString(),
       path: request.url,
-      message: exception.message,
+      message: message,
+      error: error,
     };
-    response.status(status).json(errorResponse);
+    response.status(status).json(customErrorResponse);
   }
 }
