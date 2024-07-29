f499b14acd3b81d1a347831055c2ac497c418451
jmvsevilleja <jesss@2damcreative.com>
2024-07-29 16:11:33 +0800

    added swagger package

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,19 @@ import { HttpExceptionFilter } from './common/filter/http-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptor/wrap-response.interceptor';
 import { TimeoutInterceptor } from './common/interceptor/timeout.interceptor';
 // import { ApiKeyGuard } from './common/guard/api-key.guard';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('I Love Coffee')
+    .setDescription('Coffee Application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
   // global scope
   /* This code snippet is setting up a global validation pipe in a Nest.js application. Here's what each
 option in the `ValidationPipe` configuration does: */
@@ -23,7 +33,10 @@ option in the `ValidationPipe` configuration does: */
   // );
   app.useGlobalFilters(new HttpExceptionFilter());
   // app.useGlobalGuards(new ApiKeyGuard())
-  app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor());
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
   await app.listen(3000);
 }
 bootstrap();
