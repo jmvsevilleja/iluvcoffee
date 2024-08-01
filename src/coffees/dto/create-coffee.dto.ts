11aa141f974ed891f356daad42479e5a312d9fab
jmvsevilleja <jesss@2damcreative.com>
2024-08-01 16:20:08 +0800

    added ApiProperty

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({
+    description: 'The name of the coffee',
+    example: 'White Coffee',
+  })
   @IsString()
   readonly title: string;
 
+  @ApiProperty({
+    description: 'The brand of a coffee',
+    example: 'Starbucks',
+  })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({
+    example: ['Chocolate', 'Vanilla'],
+  })
   @IsString({ each: true })
   readonly flavors: string[];
 }
